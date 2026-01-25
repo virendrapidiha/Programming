1afd2a8225593bc5e2da1f2b7c3a38860d8d533c
rohittripathi68316 <rohittripathi68316@gmail.com>
2026-01-25 13:14:28 +0530

    SumMessage

---
+++ java-basic/SumMessage.java
@@ -0,0 +1,8 @@
+public class SumMessage {
+      public static void main(String[]args){
+            int a =45;
+            int b=12;
+            int sum=a+b;
+            System.out.println("the sum of "+a+ "&" +b+"="+ sum);
+      }
+}
