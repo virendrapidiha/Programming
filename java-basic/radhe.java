630fb84c51d4d032f3495085763c740b12d131cf
rohittripathi68316 <rohittripathi68316@gmail.com>
2026-01-29 22:14:46 +0530

    radhe

---
+++ java-basic/radhe.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class radhe {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        double s = (a + b + c) / 2;
+        double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+        System.out.println(area);
+
+        sc.close();
+    }
+}
