c529fd03e5c341a04b70f7a54e30d52f356997ed
rohittripathi68316 <rohittripathi68316@gmail.com>
2026-02-03 23:27:25 +0530

    GreatestOfTwo

---
+++ java-basic/GreatestOfTwo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GreatestOfTwo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(a);
+        } else if (b > a) {
+            System.out.println(b);
+        } else {
+            System.out.println("Equal");
+        }
+
+        sc.close();
+    }
+}
