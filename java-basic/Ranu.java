29bac70692e809fd83637e5a97a34bb644e1ad8c
rohittripathi68316 <rohittripathi68316@gmail.com>
2026-02-11 18:46:45 +0530

    VowelOrConsonant

---
+++ java-basic/Ranu.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an alphabet: ");
+        char ch = sc.next().charAt(0);
+
+        
+        ch = Character.toLowerCase(ch);
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.println("It is a Vowel.");
+            } else {
+                System.out.println("It is a Consonant.");
+            }
+        } else {
+            System.out.println("Invalid input! Please enter an alphabet.");
+        }
+
+        sc.close();
+    }
+}
