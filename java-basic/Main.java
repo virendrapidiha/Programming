222c8caecb15a4cc97f6e070ef5618fd7c625c0d
rohittripathi68316 <rohittripathi68316@gmail.com>
2026-02-14 09:50:35 +0530

    Main

---
+++ java-basic/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();  // Accept integer input
+
+        if (n > 0) {
+            for (int i = 1; i <= n; i++) {
+                System.out.println("Hello World");
+            }
+        } else {
+            System.out.println("Invalid input");
+        }
+
+        sc.close();
+    }
+}
